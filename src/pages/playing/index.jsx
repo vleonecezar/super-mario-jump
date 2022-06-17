70cc1b4ffe6646028968c77c448b4ae33d83f857
vleonecezar <vleone.social@gmail.com>
2022-06-17 05:10:38 -0300

    estilo, animações e sistema de colisão terminado

---
+++ src/pages/playing/index.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { PlayingContainer } from "./styled";
+
+import mario_img from "../../assets/mario-walking.gif";
+import mario_jump_img from "../../assets/mario-jump.gif";
+import mario_dying_img from "../../assets/mario-dying.gif";
+import bullet_img from "../../assets/bullet-bill.png";
+import bushes_img from "../../assets/bushes.png";
+import clouds_img from "../../assets/clouds.png";
+
+const Playing = () => {
+  const [gameOver, setGameOver] = useState(false);
+  const [jump, setJump] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const mario = useRef();
+  const bullet = useRef();
+  const bushes = useRef();
+  const clouds = useRef();
+
+  const getElementPosition = (element) => {
+    if (element.className.includes("mario")) {
+      return +window.getComputedStyle(element).bottom.replace("px", "");
+    } else {
+      return +window.getComputedStyle(element).left.replace("px", "");
+    }
+  };
+
+  const stopAnimation = (element, position) => {
+    element.style.animation = "none";
+
+    if (element.className.includes("mario")) {
+      element.style.bottom = `${position}px`;
+    } else {
+      element.style.left = `${position}px`;
+    }
+  };
+
+  const handleJump = (event, eventName) => {
+    if (event.code === "Space" || eventName === "touchstart") {
+      setJump(true);
+
+      setTimeout(() => {
+        setJump(false);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    const loop = setInterval(() => {
+      const marioPosition = getElementPosition(mario.current);
+      const bulletPosition = getElementPosition(bullet.current);
+      const bushesPosition = getElementPosition(bushes.current);
+      const cloudsPosition = getElementPosition(clouds.current);
+
+      if (
+        marioPosition <= 130 &&
+        bulletPosition <= 70 &&
+        bulletPosition >= -90
+      ) {
+        stopAnimation(clouds.current, cloudsPosition);
+        stopAnimation(bushes.current, bushesPosition);
+        stopAnimation(bullet.current, bulletPosition);
+        stopAnimation(mario.current, marioPosition);
+
+        setGameOver(true);
+        clearInterval(loop);
+      }
+    }, 10000); //10
+  }, []);
+
+  useEffect(() => {
+    const events = ["keydown", "touchstart"];
+
+    events.forEach((event) => {
+      window.addEventListener(event, (e) => handleJump(e, event));
+    });
+
+    const loop = setInterval(() => {
+      if (!gameOver) {
+        setScore((score) => (score += 1));
+      }
+    }, 2000);
+
+    return () => {
+      events.forEach((event) => {
+        window.removeEventListener(event, (e) => handleJump(e, event));
+      });
+
+      clearInterval(loop);
+    };
+  }, [gameOver]);
+
+  return (
+    <PlayingContainer>
+      <span>SCORE: {score}</span>
+
+      <img ref={bushes} src={bushes_img} className="bushes" alt="bushes" />
+
+      <img ref={clouds} src={clouds_img} className="clouds" alt="clouds" />
+      {!gameOver ? (
+        <img
+          className={!jump ? "mario" : "mario jump"}
+          ref={mario}
+          src={!jump ? mario_img : mario_jump_img}
+          alt="mario"
+        />
+      ) : (
+        <img
+          className="mario dying"
+          ref={mario}
+          src={mario_dying_img}
+          alt="mario"
+        />
+      )}
+
+      <img className="bullet" ref={bullet} src={bullet_img} alt="bullet bill" />
+    </PlayingContainer>
+  );
+};
+
+export default Playing;
---
+++ src/pages/playing/styled.js
@@ -0,0 +1,128 @@
+import styled from "styled-components";
+
+import background_playing from "../../assets/background-playing.webp";
+
+export const PlayingContainer = styled.div`
+  background: url(${background_playing}) center no-repeat;
+  background-size: cover;
+
+  height: 100%;
+  border-bottom: 11px solid #ca945d;
+  box-shadow: inset 0px -2px #04ce3b;
+  position: relative;
+
+  overflow: hidden;
+
+  animation: screen_transition 0.5s;
+
+  @keyframes screen_transition {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  span {
+    position: relative;
+    z-index: 100;
+    display: block;
+    padding: 5px 0 0 5px;
+
+    color: black;
+    font-size: 1em;
+    font-weight: bolder;
+  }
+
+  .clouds {
+    width: 100%;
+    max-width: 340px;
+    filter: opacity(90%);
+
+    position: absolute;
+    top: 10px;
+
+    animation: clouds_animation 25s infinite linear;
+
+    @keyframes clouds_animation {
+      from {
+        left: 600px;
+      }
+      to {
+        left: -400px;
+      }
+    }
+  }
+
+  .bushes {
+    width: 100%;
+    max-width: 404px;
+
+    position: absolute;
+    bottom: 2px;
+
+    animation: bushes_animation 4s infinite linear;
+
+    @keyframes bushes_animation {
+      from {
+        left: 800px;
+      }
+      to {
+        left: -400px;
+      }
+    }
+  }
+
+  .mario {
+    width: 100%;
+    max-width: 40px;
+
+    position: absolute;
+    bottom: 0px;
+    left: 50px;
+
+    z-index: 200;
+  }
+
+  .dying {
+    max-width: 45px;
+  }
+
+  .jump {
+    animation: jump_animation 1s ease-out;
+
+    @keyframes jump_animation {
+      0% {
+        bottom: 0;
+      }
+      50% {
+        bottom: 200px;
+      }
+      100% {
+        bottom: 0;
+      }
+    }
+  }
+
+  .bullet {
+    width: 100%;
+    max-width: 140px;
+
+    position: absolute;
+    bottom: 10px;
+
+    z-index: 100;
+
+    animation: bullet_animation 2s infinite linear;
+
+    @keyframes bullet_animation {
+      from {
+        left: 800px;
+      }
+      to {
+        left: -400px;
+      }
+    }
+  }
+`;
